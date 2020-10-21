c88d07b06933b0e35630f06424d91972986de5f0
gksgks0726 <gksgks0726@naver.com>
2020-10-21 21:57:01 +0900

    hw1

---
+++ hw1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    
+     int a;
+
+     printf("정수");
+     scanf("%d", &a);
+     printf("정수출력: %d\n\n", a);
+
+     double b = 0.0;
+
+     printf("실수");
+     scanf(" %lf", &b); // 실수출력에는 lf 기억하기
+     printf("실수출력: %f\n\n", b);
+
+     char ch1, ch2, str[20];
+     printf("문자1 ");
+     scanf(" %c", &ch1);
+     printf("문자2 ");
+     scanf(" %c", &ch2);
+     
+
+     printf("입력문자: %c %c\n\n", ch1, ch2);
+
+     printf("문자열 ");
+     scanf("%s", str);
+     printf("문자열: %s", str);
+
+     return 0;
+     
+
+}
